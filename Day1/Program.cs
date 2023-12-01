78820dbe9be9178a87ef1532dbe0b0cd9b84c1fd
Srivatsn Narayanan <srivatsn@users.noreply.github.com>
2023-12-01 23:02:36 +0000

    Day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,8 +1,67 @@
-﻿if (args.Length > 0)
+﻿var input = File.ReadAllText(args.Length > 0 ? args[0] : "input.txt");
+var lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+var words = new Dictionary<string, int>(){
+    {"one", 1},
+    {"two", 2},
+    {"three", 3},
+    {"four", 4},
+    {"five", 5},
+    {"six", 6},
+    {"seven", 7},
+    {"eight", 8},
+    {"nine", 9},
+    {"0", 0},
+    {"1", 1},
+    {"2", 2},
+    {"3", 3},
+    {"4", 4},
+    {"5", 5},
+    {"6", 6},
+    {"7", 7},
+    {"8", 8},
+    {"9", 9}
+};
+
+
+int FindFirstNumber(string line)
 {
-    Console.WriteLine("Hello, " + args[0] + "!");
-}
-else
+for (int i = 0; i < line.Length; i++)
 {
-    Console.WriteLine("Hello, World!");
+var subStr = line.Substring(i);
+var first = words.Select(w => subStr.StartsWith(w.Key, StringComparison.OrdinalIgnoreCase) ? w.Value : -1)
+    .SingleOrDefault(w => w > 0);
+if (first > 0)
+return first;
 }
+return -1;
+}
+
+int FindLastNumber(string line)
+{
+    for (int i = line.Length - 1; i >= 0; i--)
+    {
+        var subStr = line.Substring(i);
+        var first = words.Select(w => subStr.StartsWith(w.Key, StringComparison.OrdinalIgnoreCase) ? w.Value : -1)
+            .SingleOrDefault(w => w > 0);
+        if (first > 0)
+            return first;
+    }
+    return -1;
+}
+
+int sum = 0;
+foreach (var line in lines)
+{
+    var valueString = FindFirstNumber(line).ToString() + FindLastNumber(line).ToString();
+    if (!int.TryParse(valueString, out int value))
+    {
+        Console.WriteLine($"Failed to parse value from line: {line}");
+        return;
+    }
+    Console.WriteLine($"Value: {value}");
+    sum += value;
+}
+
+Console.WriteLine($"Sum of all values: {sum}");
+
