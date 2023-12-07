ec1831a6a656b06b7e5288656cf5e6542ea7f603
Srivatsn Narayanan <srivatsn@users.noreply.github.com>
2023-12-07 20:37:47 +0000

    Add day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+var input = File.ReadAllText(args.Length > 0 ? args[0] : "input.txt");
+var lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
