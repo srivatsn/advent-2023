64dc4f9ac2dc1ea3c2b68bece96c51b85f770d82
Srivatsn Narayanan <srivatsn@users.noreply.github.com>
2023-12-08 05:55:29 +0000

    Add day4 scaffold

---
+++ Day4/Program.cs
@@ -0,0 +1,4 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllText(args.Length > 0 ? args[0] : "input.txt");
+var lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
