614539ac99eb4a40dcf14daead1473836685b612
Srivatsn Narayanan <srivatsn@users.noreply.github.com>
2023-12-17 08:18:43 +0000

    Day15

---
+++ Day15/Program.cs
@@ -0,0 +1,22 @@
+﻿var input = File.ReadAllText(args.Length > 0 ? args[0] : "input.txt");
+var words = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+int sum = 0;
+foreach (var word in words)
+{
+    sum += Hash(word);
+}
+
+Console.WriteLine(sum);
+
+static int Hash(string word)
+{
+    int hash = 0;
+    foreach (var c in word)
+    {
+        hash += c;
+        hash *= 17;
+        hash %= 256;
+    }
+    return hash;
+}
